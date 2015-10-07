cd0a52aaa7fe19b4b26847d393445126ebd2b092
xps-15.04 <wuhao@pdx.edu>
2015-10-06 18:00:44 -0700

    next: step3

---
+++ assignment.c
@@ -0,0 +1,17 @@
+#include "assignment.h"
+
+extern struct thread* current_thread;
+extern struct thread* inactive_thread;
+
+int factorial(int n){
+	return n == 0 ? 1 : n * factorial(n-1);
+};
+
+void fun_with_threads(void* arg){
+	int n = *(int*) arg;
+	printf("%d!= %d\n", n, factorial(n));
+};
+
+void thread_wrap(){
+	current_thread->initial_function(current_thread->initial_argument);
+};
